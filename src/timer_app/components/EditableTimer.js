c1f6ac5f89a45a4454c42939f1269e4e293244d3
Gabriel Rockson <thegabrielrockson@gmail.com>
2021-09-04 22:46:07 +0000

    Add state to the EditableTimer component

--- src/timer_app/components/EditableTimer.js
+++ src/timer_app/components/EditableTimer.js
@@ -4,10 +4,14 @@ import TimerForm from "./TimerForm";
 
 
 class EditableTimer extends React.Component {
+  state = {
+    editFormOpen: false,
+  }
   render() {
-    if (this.props.editFormOpen) {
+    if (this.state.editFormOpen) {
       return (
         <TimerForm 
+          id={this.props.id}
           title={this.props.title}
           project={this.props.project}
         />
@@ -15,6 +19,7 @@ class EditableTimer extends React.Component {
     } else {
       return (
         <Timer
+          id={this.props.id}
           title={this.props.title}
           project={this.props.project}
           elapsed={this.props.elapsed}
